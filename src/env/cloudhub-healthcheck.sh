50258a5bb45e2878eb2e47ba228a55581456f697
Danny <danny@zennify.com>
2021-02-25 16:10:26 -0500

    add healthcheck

---
+++ src/env/cloudhub-healthcheck.sh
@@ -0,0 +1,26 @@
+
+counter=0
+tick=10
+timeout=300
+
+sleep $tick
+
+until [ $counter -gt $timeout ]
+do
+  status=$(anypoint-cli runtime-mgr cloudhub-application describe $repoName -o json | jq -r '.Status')
+  if [ $status == "STARTED" ]; then
+    echo 'Deployment has started on cloudhub.'
+    exit 0
+    break
+  elif [[ $status == "DEPLOYING" || $status == "UPDATING" ]]; then
+    (( counter = counter + tick ))
+    echo 'Currently still deploying or updating.'
+    sleep $tick
+  else
+    echo 'Unexpected state detected! Check deployment at cloudhub for more details.'
+    exit 1
+  fi
+done
+
+echo 'Script exceeded set timeout.'
+exit 1
