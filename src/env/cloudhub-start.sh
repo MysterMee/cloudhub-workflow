5a64bf8464d92e1fc3855ede4c88177cbf124995
Danny <danny@zennify.com>
2021-02-26 12:28:19 -0500

    test

--- src/env/cloudhub-start.sh
+++ src/env/cloudhub-start.sh
@@ -1,6 +1,6 @@
 
 status=`anypoint-cli runtime-mgr cloudhub-application describe "$@" -o json | jq -r '.Status'`
-if [ "$status" = "UNDEPLOYED" ]; then
+if [ "$status" == "UNDEPLOYED" ]; then
   echo "$@ was detected to be in UNDEPLOY state, so starting the application first."
   anypoint-cli runtime-mgr cloudhub-application start "$@"
 fi
