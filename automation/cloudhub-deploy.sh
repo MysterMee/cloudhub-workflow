5d9d2cbc41ff2710e2842dc5bf7ff85fa00ebbfd
Danny <danny@zennify.com>
2021-03-01 15:19:30 -0500

    generalize github runner script

--- automation/cloudhub-deploy.sh
+++ automation/cloudhub-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+# Anypoint CLI's deploy command errors out when the application already exists
+#   Simply attempt modify when deploy returns an error
 anypoint-cli runtime-mgr cloudhub-application deploy "$@" ||
 anypoint-cli runtime-mgr cloudhub-application modify "$@"
